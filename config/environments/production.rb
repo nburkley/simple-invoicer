d4f87eef590454201b6a4c5d5264c9372a271083
Niall Burkley <niallburkley@gmail.com>
2012-09-19 13:35:30 +0800

    more mail configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,13 @@ SimpleInvoicer::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'boiling-garden-6763.heroku.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
